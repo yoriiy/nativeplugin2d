3b64ef1472a359ef9790f80abe5d9cc88304754b
SAITO-PC\Yoriy <hogsleik@gmail.com>
2017-10-24 16:41:23 +0900

    スクリプトビュー追加

---
+++ Assets/Editor/ScriptViewWindow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class ScriptViewWindow : EditorWindow {
+
+	[MenuItem("Window/ScriptViewWindow")]
+    public static void OpenWindow()
+    {
+        EditorWindow.GetWindow<ScriptViewWindow>();
+    }
+}
