b7c8014188297cc202a4e05f6a8a3858bd2de182
SAITO-PC\Yoriy <hogsleik@gmail.com>
2017-11-07 10:18:03 +0900

    phpファイルを上げました。

---
+++ Assets/Scripts/PHPUpload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PHPUpload : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        var url = "http://test.php";
+
+        var wwwForm = new WWWForm();
+
+        wwwForm.AddField("key1", "xxx");
+
+        var req = new WWW(url, wwwForm);
+
+        yield return req;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
