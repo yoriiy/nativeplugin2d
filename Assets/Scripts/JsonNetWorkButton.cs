040328bac3f4f9deadeb52a823ec09942cfb2330
SAITO-PC\Yoriy <hogsleik@gmail.com>
2017-11-08 22:49:04 +0900

    PHPでのサーバー内のjsonを書き換えられるように修正しました。

---
+++ Assets/Scripts/JsonNetWorkButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonNetWorkButton : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnClickButton () {
+        StartCoroutine(JsonNetButton());
+	}
+	
+	// Update is called once per frame
+	private IEnumerator JsonNetButton() {
+        // 送信
+        string url = "http://hoguhogu.sakura.ne.jp/ranking.php";
+        WWW www = new WWW(url);
+        yield return www; //受信
+        Debug.Log(www.text); //表示
+    }
+}
--- Assets/Scripts/PHPButton.cs
+++ Assets/Scripts/PHPButton.cs
@@ -12,19 +12,35 @@ public class PHPButton : MonoBehaviour {
     private IEnumerator CPHP()
     {
         //送信
-        string url = "http://github.com/yoriiy/nativeplugin2d/raw/master/Assets/aa.php"; //送信するphp
+        string url = "http://hoguhogu.sakura.ne.jp/aa.php"; //送信するphp
         WWWForm wwwForm = new WWWForm();
         wwwForm.AddField("count", 10);//カウントする数 +10
         WWW www = new WWW(url, wwwForm);
         yield return www;//送信
 
         //受信
-        string url2 = "http://github.com/yoriiy/nativeplugin2d/raw/master/Assets/ap.php";//受信する(表示している)php
+        string url2 = "http://hoguhogu.sakura.ne.jp/ap.php";//受信する(表示している)php
         WWW www2 = new WWW(url2);
         yield return www2; //受信
         Debug.Log(www2.text); //表示
 
         GameObject count = GameObject.Find("Count").gameObject;
         count.GetComponent<Text>().text = www2.text;
+        
+        /*string url = "http://localhost:8000/unity_test.php";
+
+        //WWWForm:WWWクラスを使用してwebサーバにポストするフォームデータを生成するヘルパークラス
+        WWWForm wwwForm = new WWWForm();
+
+        //AddFieldでfieldに値を格納                
+        wwwForm.AddField("text", "テキストだよー");
+
+        //WWWオブジェクトにURL,WWWFormをセットすることでPOST,GETを行える。
+        WWW www = new WWW(url, wwwForm);
+
+        //実行
+        yield return www;
+
+        Debug.Log(www.text); //表示*/
     }
 }
