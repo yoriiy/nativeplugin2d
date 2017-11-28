c57e568bae79dac53d28904f6816c62753e9181f
Yoriy <hogsleik@gmail.com>
2017-11-28 22:36:07 +0900

    ファイル読み込みと書き込みを追加

---
+++ Assets/Scripts/TextDataManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class TextDataManager : MonoBehaviour {
+
+    /// <summary>
+    /// ファイル書き込み
+    /// </summary>
+    /// <param name="Path">ファイルパス</param>
+    /// <param name="Text">書き込み文字列</param>
+    /// <returns></returns>
+	public bool SaveText(string Path, string Text)
+    {
+        // ファイル書き込み
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(Application.dataPath + Path, false))
+            {
+                writer.Write(Text); // ファイルstring型書き込み
+                writer.Flush();     // ファイル更新
+                writer.Close();     // ファイルを閉じる
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// ファイル読み込み
+    /// </summary>
+    /// <param name="Path">ファイルパス</param>
+    /// <returns></returns>
+    public string ReadText(string Path)
+    {
+        string strStream = "";
+
+        // ファイル読み込み
+        try
+        {
+            using (StreamReader sr = new StreamReader(Application.dataPath + Path))
+            {
+                strStream = sr.ReadToEnd(); // ファイルstring型読み込み
+                sr.Close(); // ファイルを閉じる
+            }
+        }catch(Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        return strStream;
+    }
+}
+
+
---
+++ Assets/Scripts/TextSave.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TextSave : MonoBehaviour {
+
+    public TextDataManager TdManager;
+
+    // Use this for initialization
+    void Start () {
+        TdManager = this.transform.GetComponent<TextDataManager>();
+
+        string text = TdManager.ReadText("/pcparam.txt");
+
+        // 改行ごとに文字列を格納
+        string[] lines = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            // 改行時の文字列""(EOF)は無視
+            if (lines[i] != "")
+            {
+                string ifobj = "InputField" + (i + 1);
+                // =より後ろの値を取得
+                string[] value = lines[i].Split(new string[] { "=" }, StringSplitOptions.None);
+                Transform IFObj = GameObject.Find(ifobj).transform;
+                InputField tx = IFObj.GetComponent<InputField>();
+                // 表示するテキストに設定
+                tx.text = value[1].ToString();
+                Debug.Log(value[0] + ":" + value[1]);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    ///  データ保存
+    /// </summary>
+    public void DataSave()
+    {
+        string save_data = "";
+
+        string text = TdManager.ReadText("/pcparam.txt");
+
+        // 改行ごとに文字列を格納
+        string[] lines = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            // 改行時の文字列""(EOF)は無視
+            if (lines[i] != "")
+            {
+                string ifobj = "InputField" + (i + 1);
+                // =より前を識別名　後ろを値として取得
+                string[] value = lines[i].Split(new string[] { "=" }, StringSplitOptions.None);
+                Transform IFObj = GameObject.Find(ifobj).transform;
+                InputField tx = IFObj.GetComponent<InputField>();
+                // テキストファイル用に値を文字列で保存
+                save_data += value[0] + "=";
+                save_data += tx.text + "\r\n";
+            }
+        }
+
+        // ファイル保存
+        TdManager.SaveText("/pcparam.txt", save_data);
+    }
+}
